b264e3cf7eee82d819a1aff204a040c4a65eae36
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-02-23 12:26:39 +0530

    flatten.cjs
    modifiyng condition and adding concating array

--- flatten.cjs
+++ flatten.cjs
@@ -1,26 +1,24 @@
-let flattenArray = [];
-
-function flatten(elements, dep) {
+function flatten(elements, depth) {
     // Flattens a nested array (the nesting can be to any depth).
     // Hint: You can solve this using recursion.
     // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 
-    let depth = dep;
     if(depth === undefined){
         depth = 1;
     }
+
+    let flattenArray = [];
+
     if(Array.isArray(elements) === false){
-        return undefined;
+        return [];
     }else{
         for(let index = 0; index < elements.length; index++){
             if(Array.isArray(elements[index]) === true && depth !== 0){
-                flatten(elements[index], depth - 1);
-            }else if(Array.isArray(elements[index]) === false){
-                flattenArray.push(elements[index]);
-            }else if(Array.isArray(elements[index]) === true && depth === 0){
-                flattenArray.push(elements[index]);
-            }else{
+                flattenArray = flattenArray.concat(flatten(elements[index], depth - 1));
+            }else if(elements[index] === undefined || elements[index] === null){
                 continue;
+            }else{
+                flattenArray.push(elements[index]);
             }
         }
         return flattenArray;
