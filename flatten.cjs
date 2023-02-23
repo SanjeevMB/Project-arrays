c5510fa8d43fb916c1d5bebd3847dd9119cf8c66
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-02-23 11:07:00 +0530

    flatten.cjs
    adding depth parameter and some condition

--- flatten.cjs
+++ flatten.cjs
@@ -1,18 +1,26 @@
 let flattenArray = [];
 
-function flatten(elements) {
+function flatten(elements, dep) {
     // Flattens a nested array (the nesting can be to any depth).
     // Hint: You can solve this using recursion.
     // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 
+    let depth = dep;
+    if(depth === undefined){
+        depth = 1;
+    }
     if(Array.isArray(elements) === false){
         return undefined;
     }else{
         for(let index = 0; index < elements.length; index++){
-            if(Array.isArray(elements[index])){
-                flatten(elements[index]);
-            }else{
+            if(Array.isArray(elements[index]) === true && depth !== 0){
+                flatten(elements[index], depth - 1);
+            }else if(Array.isArray(elements[index]) === false){
                 flattenArray.push(elements[index]);
+            }else if(Array.isArray(elements[index]) === true && depth === 0){
+                flattenArray.push(elements[index]);
+            }else{
+                continue;
             }
         }
         return flattenArray;
--- test/testFlatten.cjs
+++ test/testFlatten.cjs
@@ -2,4 +2,6 @@ let flatten = require('../flatten.cjs');
 
 const nestedArray = [1, [2], [[3]], [[[4]]]];
 
-console.log(flatten(nestedArray));
+let result = flatten(nestedArray, 3);
+
+console.log(result);
