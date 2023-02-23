1fc5c8b149c37179e666f94e80d3d44a3b58b35c
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-02-23 10:00:13 +0530

    flatten.cjs
    when argument is not array

--- flatten.cjs
+++ flatten.cjs
@@ -5,14 +5,18 @@ function flatten(elements) {
     // Hint: You can solve this using recursion.
     // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 
-    for(let index = 0; index < elements.length; index++){
-        if(Array.isArray(elements[index])){
-            flatten(elements[index]);
-        }else{
-            flattenArray.push(elements[index]);
+    if(Array.isArray(elements) === false){
+        return undefined;
+    }else{
+        for(let index = 0; index < elements.length; index++){
+            if(Array.isArray(elements[index])){
+                flatten(elements[index]);
+            }else{
+                flattenArray.push(elements[index]);
+            }
         }
+        return flattenArray;
     }
-    return flattenArray;
 }
 
 module.exports = flatten;
